f992e358f9da47466e1626395af026ac44db0c77
Yevhen Borysevych <drunkedsneakers@gmail.com>
2017-05-20 23:51:25 +0300

    Create a blog

---
+++ views/articles.php
@@ -0,0 +1,29 @@
+<!DOCTIPE html>
+<hlml>
+    <head>
+    <meta charset="utf8">
+       <title>Мой первый блог</title> 
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+    </head> 
+    <body>
+        <div class="container">
+            <h1>Мой первый блог</h1>
+            <div>
+                <?php foreach($articles as $a): ?>
+                <div class="article">
+                    <h3>
+                        <a href="article.php?id=<?=$a['id']?>"><?=$a['title']?></a>
+                    </h3>
+                    <em>Опубликовано: <?=$a['date']?></em>
+                    <p><?=$a['content']?></p>               
+                </div>
+                <?php endforeach ?>
+            </div>
+            <footer>
+                <p>Мой первый Блог <br>Copyright &copy; 2015</p>
+            </footer>
+        </div>
+
+    </body>
+</hlml>
